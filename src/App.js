204d4cf5b5b538bdecb90345df75c12e177283a5
Bhavishay Chawla <bhavishay97@outlook.com>
2020-07-15 21:50:01 +0530

    added Clarifai API for face detection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import Clarifai from 'clarifai';
 import ParticlesBackground from './components/ParticlesBackground';
 import Navbar from './components/Navbar';
 import Logo from './components/Logo';
@@ -6,9 +7,34 @@ import ImageLinkForm from './components/ImageLinkForm';
 import FaceDetection from './components/FaceDetection';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_CLARIFAI_API,
+});
+
 function App() {
   const [input, setInput] = useState('');
   const [imageUrl, setImageUrl] = useState('');
+  const [box, setBox] = useState({});
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data['outputs'][0]['data']['regions'][0].region_info.bounding_box;
+
+    const image = document.querySelector('#input-image');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  const faceBox = (box) => {
+    setBox(box);
+  };
 
   const handleInputChange = (e) => {
     setInput(e.target.value);
@@ -17,6 +43,16 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setImageUrl(input);
+
+    app.models
+      .initModel({
+        id: Clarifai.FACE_DETECT_MODEL,
+      })
+      .then((generalModel) => {
+        return generalModel.predict(input);
+      })
+      .then((response) => faceBox(calculateFaceLocation(response)))
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -28,7 +64,7 @@ function App() {
         onInputChange={handleInputChange}
         onDetectSubmit={handleSubmit}
       />
-      <FaceDetection image={imageUrl} />
+      <FaceDetection image={imageUrl} box={box} />
     </Fragment>
   );
 }
--- src/components/FaceDetection/index.jsx
+++ src/components/FaceDetection/index.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import './style.css';
 
-function FaceDetection({ image }) {
+function FaceDetection({ image, box }) {
+  const boxStyles = {
+    top: box.topRow,
+    right: box.rightCol,
+    bottom: box.bottomRow,
+    left: box.leftCol,
+  };
+
   return (
-    <div className='container my-5'>
-      <div className='row'>
-        <div className='col text-center'>
-          <img
-            src={image}
-            className='img-fluid rounded'
-            id='input-image'
-            alt={image ? 'face detection image' : ''}
-          />
+    <div className='container'>
+      <div className='row my-5'>
+        <div className='col'>
+          <div className='d-flex justify-content-center'>
+            <div className='position-absolute'>
+              <img
+                src={image}
+                className='img-fluid rounded'
+                id='input-image'
+                alt={image ? 'detection image/please enter correct URL' : ''}
+              />
+              <div className='bounding-box' style={boxStyles}></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
