c0dbbe9e00f40a894a46e83256fd7e86812dac38
Bhavishay Chawla <bhavishay97@outlook.com>
2020-08-03 00:00:02 +0530

    Update navbar
    - Navbar items now appear on the right side.
    - Navbar items' text is now bolder.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,40 +3,26 @@ import { NavLink, Link } from 'react-router-dom';
 
 function Navbar({ user }) {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light'>
+    <nav className='navbar navbar-expand navbar-light'>
       <Link className='navbar-brand text-capitalize' to='/'>
         Face detector
       </Link>
-      <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarNav'
-        aria-controls='navbarNav'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
-      >
-        <span className='navbar-toggler-icon' />
-      </button>
-      <div className='collapse navbar-collapse'>
-        <div className='navbar-nav'>
-          {!user && (
-            <Fragment>
-              <NavLink className='nav-item nav-link ' to='/register'>
-                Register
-              </NavLink>
-              <NavLink className='nav-item nav-link ' to='/login'>
-                Log in
-              </NavLink>
-            </Fragment>
-          )}
-
-          {user && (
-            <NavLink className='nav-item nav-link ' to='/logout'>
-              Log out
+      <div className='navbar-nav ml-auto font-weight-bold'>
+        {!user && (
+          <Fragment>
+            <NavLink className='nav-item nav-link' to='/register'>
+              Register
             </NavLink>
-          )}
-        </div>
+            <NavLink className='nav-item nav-link' to='/login'>
+              Log in
+            </NavLink>
+          </Fragment>
+        )}
+        {user && (
+          <NavLink className='nav-item nav-link ' to='/logout'>
+            Log out
+          </NavLink>
+        )}
       </div>
     </nav>
   );
