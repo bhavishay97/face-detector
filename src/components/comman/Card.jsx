b080feb261ce77f20388a119e4e5050d8504a2cb
Bhavishay Chawla <bhavishay97@outlook.com>
2020-08-05 19:26:54 +0530

    Update Card.jsx
    - Improved card size on medium and large displays.

--- src/components/comman/Card.jsx
+++ src/components/comman/Card.jsx
@@ -4,7 +4,7 @@ function Card({ title, children }) {
   return (
     <div className='container mt-5'>
       <div className='row'>
-        <div className='col-md-4 offset-md-4'>
+        <div className='col-md-6 offset-md-3 col-lg-4 offset-lg-4'>
           <div className='card bg-transparent shadow-lg'>
             <div className='card-body'>
               <h5 className='card-title text-center'>{title}</h5>
